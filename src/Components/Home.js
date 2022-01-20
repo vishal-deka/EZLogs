195d98ec8bff072179b3884b4351df2a9e619c86
Vishal <immortal1vishal.vd@gmail.com>
2022-01-20 20:26:51 +0530

    updated home page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,23 +1,25 @@
-import { Button } from "@mui/material";
 import "./Home.css";
 import { useHistory } from "react-router-dom";
+import background from "./bg.jpg";
 
 export default function Home() {
   const his = useHistory();
   return (
-    <div className="home">
-      <div className="head">
-        <h1>Welcome to EZLogs</h1>
-      </div>
-      <div className="para">
-        <p>An easy way to keep track of your literature review.</p>
-      </div>
-      <div className="actions">
-        <div className="btn">
-          <button onClick={() => his.push("/papers")}>View Papers</button>
+    <div className="outer" style={{ backgroundImage: `url(${background})` }}>
+      <div className="home">
+        <div className="head">
+          <h1>Welcome to EZLogs</h1>
         </div>
-        <div className="btn">
-          <button onClick={() => his.push("/add")}>Add Papers</button>
+        <div className="para">
+          <p>An easy way to keep track of your literature review.</p>
+        </div>
+        <div className="actions">
+          <div className="btn">
+            <button onClick={() => his.push("/papers")}>View Papers</button>
+          </div>
+          <div className="btn">
+            <button onClick={() => his.push("/add")}>Add Papers</button>
+          </div>
         </div>
       </div>
     </div>
